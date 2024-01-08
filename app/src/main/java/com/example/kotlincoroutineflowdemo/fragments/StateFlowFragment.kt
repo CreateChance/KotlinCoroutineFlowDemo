604e0fbbc69fad650ccdd8de22c4826705360ad3
createchance <createchance@163.com>
2024-01-08 11:26:24 +0800

    完成 stateflow case

--- app/src/main/java/com/example/kotlincoroutineflowdemo/fragments/StateFlowFragment.kt
+++ app/src/main/java/com/example/kotlincoroutineflowdemo/fragments/StateFlowFragment.kt
@@ -5,10 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
 import com.example.kotlincoroutineflowdemo.databinding.FragmentStateFlowBinding
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
 class StateFlowFragment : Fragment() {
     private var viewBinding: FragmentStateFlowBinding? = null
+
+    private val viewModel by viewModels<NumberViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,11 +23,39 @@ class StateFlowFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         viewBinding = FragmentStateFlowBinding.inflate(inflater, container, false)
+
+        lifecycleScope.launch {
+            viewBinding?.apply {
+                btnAdd.setOnClickListener {
+                    viewModel.add()
+                }
+                btnMinus.setOnClickListener {
+                    viewModel.minus()
+                }
+                viewModel.number.collect {
+                    tvNumber.text = it.toString()
+                }
+            }
+        }
+
         return viewBinding?.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         viewBinding = null
+    }
+}
+
+class NumberViewModel : ViewModel() {
+
+    val number = MutableStateFlow(0)
+
+    fun add() {
+        number.value++
+    }
+
+    fun minus() {
+        number.value--
     }
 }
